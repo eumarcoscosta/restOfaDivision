c9c6322b9b9fda7009a8e01b876fb80da9b7a875
Marcos Costa <marcoscosta@air-de-marcos.lan>
2024-07-09 17:25:54 +0100

    Complete app

---
+++ restOfaDivision/restOfaDivision/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x, y, i = 0, soma = 0;
+
+            Console.WriteLine("Insira os valores abaixo: ");
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Insira um numero positivo");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+            }
+
+            int minX = Math.Min(x, y);
+            int maxX = Math.Max(x, y);
+
+            for (i = minX; i < maxX; i++)
+            {
+                if (i % 5 == 2 || i % 5 == 3)
+                {
+                    soma = i;
+                    Console.WriteLine(soma);
+                }
+            }
+        }
+    }
+}
